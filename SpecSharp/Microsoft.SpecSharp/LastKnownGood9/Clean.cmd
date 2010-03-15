4f85305643b95c11af8aabb46eaee241b9d785ed
mschwerhoff <mschwerhoff@localhost>
2010-03-15 18:39:51 +0000

    SpecSharp: Modified the cmd-scripts in LastKnownGood9/ so that they return meaningful status codes to facilitate automatic error checking.

--- SpecSharp/Microsoft.SpecSharp/LastKnownGood9/Clean.cmd
+++ SpecSharp/Microsoft.SpecSharp/LastKnownGood9/Clean.cmd
@@ -1,12 +1,2 @@
 @echo off
-
-rem set REGASM=c:\Windows\Microsoft.NET\Framework\v2.0.50727\RegAsm.exe
-set REGASM=regasm
-if not "%1"=="" set REGASM=%1
-
-rem unregister old binaries
-%REGASM% /nologo /silent /u Microsoft.VisualStudio.Package.dll
-%REGASM% /nologo /silent /u Microsoft.SpecSharp.dll
-%REGASM% /nologo /silent /u ContractsPropertyPane.dll
-rem del bin\LastKnownGood.dll
-rem del bin\LastKnownGood.pdb
+Register.cmd Clean %2
--- SpecSharp/Microsoft.SpecSharp/LastKnownGood9/RegisterLKG.cmd
+++ SpecSharp/Microsoft.SpecSharp/LastKnownGood9/RegisterLKG.cmd
@@ -1,9 +1,2 @@
 @echo off
-
-set REGASM=regasm
-if not "%1"=="" set REGASM=%1
-
-%REGASM% /nologo /codebase Microsoft.VisualStudio.Package.dll
-%REGASM% /nologo /codebase Microsoft.SpecSharp.dll
-%REGASM% /nologo /codebase ContractsPropertyPane.dll
-rem devenv /setup
+Register.cmd RegisterLKG %2
