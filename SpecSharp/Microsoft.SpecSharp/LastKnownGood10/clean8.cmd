2cf23fe83ec947b3fe1d0b243a7d76e017a16629
wuestholz <wuestholz@localhost>
2011-05-27 17:47:46 +0200

    SpecSharp: Updated and cleaned up the 'LastKnownGood10' directory.

--- SpecSharp/Microsoft.SpecSharp/LastKnownGood10/clean8.cmd
+++
@@ -1,6 +0,0 @@
-cscript /e:jscript /nologo CleanReg.js
-
-rem ..\..\RegPkg\bin\Debug\regpkg.exe /root:Software\Microsoft\VisualStudio\8.0Exp /unregister Microsoft.SpecSharp.dll
-if exist *.dll del /Q *.dll
-if exist *.pdb del /Q *.pdb
-if exist *.exe del /Q *.exe
