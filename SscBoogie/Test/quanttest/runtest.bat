3a89a6a6859219a3a502d39132036d61b5211858
wuestholz <wuestholz@localhost>
2011-12-19 23:17:54 +0100

    SscBoogie: Fixed a bug in the translation of quantifiers over arrays.
    (reported by Florian Egli)

--- SscBoogie/Test/quanttest/runtest.bat
+++ SscBoogie/Test/quanttest/runtest.bat
@@ -5,7 +5,7 @@ set BOOGIEDIR=..\..\Binaries
 set SSCEXE=%BOOGIEDIR%\ssc.exe
 set BGEXE=%BOOGIEDIR%\SscBoogie.exe
 
-for %%f in (quant0 quant1) do (
+for %%f in (quant0 quant1 quant2) do (
   echo.
   echo -------------------- %%f --------------------
   "%SSCEXE%" /target:library /debug %%f.ssc
